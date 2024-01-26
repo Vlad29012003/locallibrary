41f32b288e808b4fc39fede53e2439f88d98d0bb
Vlad <vladosmen29@gmail.com>
2024-01-26 06:39:44 +0600

    aded HTML book detail book_list and url patch

--- src/catalog/urls.py
+++ src/catalog/urls.py
@@ -1,7 +1,19 @@
-from django.urls import path
+from django.urls import path , re_path
 from . import views
 
 
 urlpatterns = [
     path('', views.index , name='index'),
+    path('books/', views.BookListView.as_view(), name='books')
+
 ]
+
+urlpatterns = [
+    path('', views.index , name='index'),
+    path('books/', views.BookListView.as_view() , name='books'),
+    re_path(r'/books/(?P<pk>\d+)$', views.BookDetailView.as_view(), name='book_detail'),
+]
+
+
+# url(r'^/url/$', views.my_reused_view, {'my_template_name': 'some_path'}, name='aurl'),
+# url(r'^/anotherurl/$', views.my_reused_view, {'my_template_name': 'another_path'}, name='anotherurl'),
--- src/catalog/views.py
+++ src/catalog/views.py
@@ -1,6 +1,10 @@
 # render() - функцию, которая генерирует HTML-файлы при помощи шаблонов страниц и соответствующих данных.
+from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render
 from .models import Book ,BookInstance ,Author ,Genre  
+from django.views import generic
+from django.http import Http404
 # Create your views here.
 
 def index(request):
@@ -19,4 +23,49 @@ def index(request):
         request,
         'index.html',
         context={'num_books':num_books,'num_instances':num_book_ins,'num_instances_available':num_instances_avalable,'num_authors':num_authors},
+    )
+
+class BookListView(generic.ListView):
+    model = Book
+    context_object_name = 'my_book_list' #  # ваше собственное имя переменной контекста в шаблоне
+    queryset = Book.objects.filter(title__icontains='war')[:5] # Получение 5 книг, содержащих слово 'war' в заголовке
+    template_name = 'books/my_arbitrary_template_name_list.html'  # Определение имени вашего шаблона и его расположения
+
+
+
+# Данный подход является более гибким
+    
+# class BookListView(generic.ListView):
+#   model = Book
+
+#   def get_queryset(self):
+#       return Book.objects.filter(title__icontains='war')[:5] # Получить 5 книг, содержащих 'war' в заголовке
+    
+    def get_context_data(self, **kwargs):
+         # В первую очередь получаем базовую реализацию контекста
+        context = super(BookListView, self).get_context_data(**kwargs)
+        # Добавляем новую переменную к контексту и инициализируем её некоторым значением
+        context['some_data'] = 'This is just some data'
+        return context
+    
+
+class BookDetailView(generic.ListView):
+    model = Book
+    paginate_by = 2
+
+      
+
+
+def book_detail_view(request,pk):
+    try:
+        book_id=Book.objects.get(pk=pk)
+    except Book.DoesNotExist:
+        raise Http404("Book does not exist")
+
+    #book_id=get_object_or_404(Book, pk=pk)
+
+    return render(
+        request,
+        'catalog/book_detail.html',
+        context={'book':book_id,}
     )
