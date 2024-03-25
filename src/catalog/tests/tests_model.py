2d46c63b2474090f36713b14c054439a324fba64
Vlad <vladosmen29@gmail.com>
2024-03-25 13:37:29 +0600

    aded a test for model Auhtor

--- src/catalog/tests/tests_model.py
+++ src/catalog/tests/tests_model.py
@@ -1,30 +1,73 @@
 from django.test import TestCase 
+from catalog.models import Author
 
 
-class YourTestCase(TestCase):
+
+class AuthorModelTest(TestCase):
     @classmethod
-    def setUpTest_data(cls):
-        print('setUpTestData: Run once to set up non-modified data for all class methods')
-        pass
+    def setUpTestData(cls):
+        #Set up non-modified objects used by all test methods
+        Author.objects.create(first_name = 'Big' , last_name = 'Bob')
+        
+    def test_first_name_label(self):
+        author = Author.objects.get(id = 1)
+        field_label = author._meta.get_field('first_name').verbose_name
+        self.assertEqual(field_label ,'first name')
 
 
-    def  setUp(self):
-        print("setUp: Run once for every test method to setup clean data.")
-        pass
+    def test_date_of_death_label(self):
+        author = Author.objects.get(id = 1)
+        field_label = author._meta.get_field('date_of_death').verbose_name
+        self.assertEqual(field_label ,'date_of_death')
 
 
-    def test_false_is_false(self):
-        print("Method: test_false_is_false.")
-        self.assertFalse(False)
+    def test_first_name_max_length(self):
+        author = Author.objects.get(id = 1)
+        max_lenght = author._meta.get_field('first_name').max_length
+        self.assertEquals(max_lenght,100)
 
 
-    def test_false_is_true(self):
-        print('Method: test_false_is_true.')
-        self.assertTrue(True)
+    def test_object_name_is_last_name_comma_first_name(self):
+        author = Author.objects.get(id = 1)
+        expected_object_name = '%s, %s' % (author.last_name, author.first_name)
+        self.assertEqual(expected_object_name,str(author))
+
+    def test_get_absolute_url(self):
+        author = Author.objects.get(id = 1)
+        self.assertEqual(author.get_absolute_url(),'/catalog/author/1')
 
 
-    def test_one_plus_one_equals_two(self):
-        print("Method: test_one_plus_one_equals_two.")
-        self.assertEqual(1 + 1 , 2)
+
+
+
+
+
+
+
+# class YourTestCase(TestCase):
+#     @classmethod
+#     def setUpTest_data(cls):
+#         print('setUpTestData: Run once to set up non-modified data for all class methods')
+#         pass
+
+
+#     def  setUp(self):
+#         print("setUp: Run once for every test method to setup clean data.")
+#         pass
+
+
+#     def test_false_is_false(self):
+#         print("Method: test_false_is_false.")
+#         self.assertFalse(False)
+
+
+#     def test_false_is_true(self):
+#         print('Method: test_false_is_true.')
+#         self.assertTrue(True)
+
+
+#     def test_one_plus_one_equals_two(self):
+#         print("Method: test_one_plus_one_equals_two.")
+#         self.assertEqual(1 + 1 , 2)
 
         
