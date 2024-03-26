2805df30aa1d16f8a2ad7d15a89b6ce2afca440f
Vlad <vladosmen29@gmail.com>
2024-03-26 11:54:48 +0600

    aded test for views AuthorListViewset

---
+++ src/catalog/tests/test_views.py
@@ -0,0 +1,60 @@
+from django.test import TestCase
+from catalog.models import Author , BookInstance, Book, Genre
+from django.contrib.auth.models import User
+from django.urls import reverse
+import datetime
+from django.utils import timezone
+
+
+class AuthorListViewset(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        number_of_authors = 13
+        for author_num in range(number_of_authors):
+            Author.objects.create(first_name='Christian %s' % author_num, last_name = 'Surname %s' % author_num,)
+
+
+    def test_view_url_exists_at_desired_location(self):
+        resp = self.client.get('/catalog/authors/')
+        self.assertEqual(resp.status_code , 200)
+
+    
+    def test_view_url_accessible_by_name(self):
+        resp = self.client.get(reverse('authors'))
+        self.assertEqual(resp.status_code, 200)
+
+
+    def test_view_uses_correct_template(self):
+        resp = self.client.get(reverse('authors'))
+        self.assertEqual(resp.status_code, 200)
+        self.assertTemplateUsed(resp , 'catalog/author_list.html')
+
+
+
+
+
+
+class LoanedBookInstancesByUserListViewTest(TestCase):
+    # Создание двух пользователей
+    def setUp(self):
+        test_user1 = User.objects.create_user(username='testuser1', password='12345')
+        test_user1.save()
+        test_user2 = User.objects.create_user(username='testuser2', password='12345')
+        test_user2.save()
+
+
+    # def test_pagination_is_ten(self):
+    #     resp = self.client.get(reverse('authors'))
+    #     self.assertEqual(resp.status_code , 200)
+    #     self.assertTrue('is_paginated' in resp.context)
+    #     self.assertTrue(resp.context['is_paginated'] == True)
+    #     self.assertTrue(resp.context(['author_list']) == 10)
+
+
+
+    # def test_lists_all_authors(self):
+    #     resp = self.client.get(reverse('authors') + '?page=2')
+    #     self.assertEqual(resp.status_code, 200)
+    #     self.assertTrue('is_paginated' in resp.context)
+    #     self.assertTrue(resp.context['is_paginated'] == True)
+    #     self.assertTrue(len(resp.context['author_lsit']) == 3)
