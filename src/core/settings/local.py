ceaac793c62fe20a51adffe61d1970a4cb80151c
Vlad <vladosmen29@gmail.com>
2024-07-01 14:03:46 +0600

    aded a dockerfile

--- src/core/settings/local.py
+++ src/core/settings/local.py
@@ -17,6 +17,6 @@ DATABASES = {
         'USER': env('POSTGRES_USER'),
         'PASSWORD': env('POSTGRES_PASSWORD'),
         'HOST': env('POSTGRES_HOST'),
-        'PORT': env('POSTGRES_PORT' , cast = int)
+        'PORT': env('POSTGRES_PORT' ,cast=int)
     }
 }
